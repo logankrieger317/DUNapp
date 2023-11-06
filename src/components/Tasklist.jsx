44add887162c94022aff48f785c0a66088381c6a
Joshua Thomas <jthoma10@hotmail.com>
2023-11-06 08:05:51 -0600

    Josh weekend

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,6 +1,37 @@
-export default function Tasklist() {
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+
+export default function TaskList() {
+
+    const [taskList, setTaskList] = useState([])
+
+    useEffect(()=>{
+        const getTasklist = async() => {
+            const response = await axios.get()
+            setTaskList(response)
+            console.log(response)
+        }
+        getTasklist()
+    },[])
+
+    let navigate = useNavigate()
+
+    const showTask = (key) => {
+        navigate(`${key}`)
+    }
+
+
     return(
-        <div>
+        <div className="taskList">
+            {
+                taskList.map((task, key) => (
+                    <div key={key} onClick={()=>showTask(key)} className="card">
+
+                    </div>
+                ))
+            }
             
         </div>
     )
