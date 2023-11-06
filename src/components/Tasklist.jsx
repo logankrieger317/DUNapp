1d3b61e004f7c03f3cdc8576c9ceec51d2a55859
Logan <logankrieger317@gmail.com>
2023-11-06 09:23:28 -0600

    m

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,37 +1,6 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
-
-
-export default function TaskList() {
-
-    const [taskList, setTaskList] = useState([])
-
-    useEffect(()=>{
-        const getTasklist = async() => {
-            const response = await axios.get()
-            setTaskList(response)
-            console.log(response)
-        }
-        getTasklist()
-    },[])
-
-    let navigate = useNavigate()
-
-    const showTask = (key) => {
-        navigate(`${key}`)
-    }
-
-
+export default function Tasklist() {
     return(
-        <div className="taskList">
-            {
-                taskList.map((task, key) => (
-                    <div key={key} onClick={()=>showTask(key)} className="card">
-
-                    </div>
-                ))
-            }
+        <div>
             
         </div>
     )
