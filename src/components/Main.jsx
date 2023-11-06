63b97185b26b060c21cb13b5802ea34c8ff34a03
Joshua Thomas <jthoma10@hotmail.com>
2023-11-06 13:02:44 -0600

    Routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import TaskList from "./TaskList"
+import TaskDetail from "./TaskDetail"
+import Home from "./Home"
+
+
+
 export default function Main() {
     return(
-        <div>
+        <div className="main">
+            <div className="routes-container">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/TaskList" element={<TaskList/>}/>
+                    <Route path="/TaskList/:id" element={<TaskDetail/>}/>
+                </Routes>
+            </div>
+
             
         </div>
     )
--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import axios from "axios"
+
+
+
 export default function TaskDetail() {
-    return(
-        <div>
-            
+
+    const [task, setTask] = useState()
+
+    let{id} = useParams()
+
+    useEffect(()=>{
+        const getTask = async() => {
+            const response = await axios.get()
+            setTask(response)
+        }
+        getTask()
+    },[])
+
+
+
+
+
+
+
+
+
+    return task ? (
+        <div className="taskDetail">
+            <h2>Task: {task.name}</h2>
+            <h4>Description: {task.description}</h4>
+            <h4>Due by: {task.time}</h4>
+            <Link to="/TaskList"> Return to Task List </Link>
         </div>
-    )
+    ) : <h3>Pulling up Task...</h3>
 }
